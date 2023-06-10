ba92d9f63ed469484fc2cd8d4c4c79e7ba3eb7dd
fevercatdream <kuma18@uw.edu>
2023-06-10 14:29:18 -0700

    updated datebase connection for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(`mongodb+srv://fevercatdream:simple@leafdream.0l1xgsy.mongodb.net/?retryWrites=true&w=majority`)
 module.exports = mongoose.connection;
