d22d6be623a3da01541058da42d55b276517b4f3
fevercatdream <kuma18@uw.edu>
2023-06-09 21:27:09 -0700

    updated searchbooks and savedbooks to use queries and mutations from apollo/client

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,8 +10,9 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -21,6 +22,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [ saveBook, { data, loading, error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,22 +68,18 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const user = await saveBook({variables: { 
+        authors: bookToSave.authors, 
+        description: bookToSave.description, 
+        title: bookToSave.title, 
+        bookId: bookToSave.bookId, 
+        image: bookToSave.image, 
+        link: bookToSave.link
+      }});
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      setSavedBookIds(user.savedBooks.map((book) => book.bookId));
     } catch (err) {
       console.error(err);
     }
