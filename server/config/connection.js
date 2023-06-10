c46606b6620c87e935644303df3c63c348a2b948
fevercatdream <kuma18@uw.edu>
2023-06-10 14:41:25 -0700

    updated connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-// local connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-// connect to heroku deployed site
-// mongoose.connect(`mongodb+srv://fevercatdream:simple@leafdream.0l1xgsy.mongodb.net/?retryWrites=true&w=majority`)
 
 module.exports = mongoose.connection;
