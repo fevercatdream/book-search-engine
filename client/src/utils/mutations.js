d22e5b3bc8a29ff62a621488ead1cabb06a35438
fevercatdream <kuma18@uw.edu>
2023-06-09 20:37:58 -0700

    updated queries.js and mutations.js to use @apollo/client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,96 +1,47 @@
 import { gql } from '@apollo/client';
 
-export const loginUser = (userData) => {
-    fetch( '/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: `
-                mutation {
-                    login(email: "${userData.email}", password: "${userData.password}"){
-                       token
-                       user {
-                        _id
-                        username
-                        email
-                        bookCount
-                        savedBooks
-                       }
-                    }
-                } 
-            `
-        })
-    })
-}
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
+        }        
+    }
+`;
 
-export const createUser = (userData) => {
-    fetch( '/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: `
-                mutation {
-                    addUser(username: "${userData.username}", email: "${userData.email}", password: "${userData.password}"){
-                        token
-                        user {
-                            _id
-                            username
-                            email
-                            bookCount
-                            savedBooks
-                        }
-                    }
-                }
-            `
-        })
-    })
-}
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
+        } 
+    }
+`;
 
-export const saveBook = (bookData, token) => {
-    fetch( '/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-            query: `
-                mutation {
-                    saveBook(authors: "${bookData.authors}", description: "${bookData.description}", title: "${bookData.title}", bookId: "${bookData.bookId}", image: "${bookData.image}", link: "${bookData.link}"){
-                        _id
-                        username
-                        email
-                        bookCount
-                        savedBooks
-                    }
-                }
-            `
-        })
-    })
-}
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: String!, $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+    }
+`;
 
-export const deleteBook = (bookId, token) => {
-    fetch( '/graphql', {
-        method: 'POST',
-        headers: {
-            authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-            query: `
-                mutation {
-                    removeBook(bookId: "${bookId}"){
-                        _id
-                        username
-                        email
-                        bookCount
-                        savedBooks
-                    }
-                }
-            `
-        })
-    })
-}
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+    }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const getMe = (token) => {
-    fetch( '/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-            query: `
-                query {
-                    me
-                }
-            `
-        })
-    })
-}
+export const GET_ME = gql`
+    query { me }
+`;
